fd2cc58df596f3e5254517550bb6aa0b1e699e43
Andrew Jones <andrew.jones@audiogum.com>
2017-12-07 17:10:42 +0000

    Start working on snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject audiogum/clj-lazy-json "0.0.3"
+(defproject audiogum/clj-lazy-json "0.0.4-SNAPSHOT"
   :description "Jackson-based lazy JSON parsing library for Clojure."
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [org.codehaus.jackson/jackson-core-asl "1.8.6"]]
