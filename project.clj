eb6671056e061b4995961bf25836d753fce712fa
Andrew Jones <andrew.jones@audiogum.com>
2017-12-07 17:05:51 +0000

    Update to latest version of Clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-lazy-json "0.0.3"
   :description "Jackson-based lazy JSON parsing library for Clojure."
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [org.codehaus.jackson/jackson-core-asl "1.8.6"]]
   ; :jvm-opts ["-Xmx512m" "-XX:+UseConcMarkSweepGC"]
   )
